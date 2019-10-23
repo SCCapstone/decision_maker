9778aff251d60fbd8f64ba68fed9205b1c3e498c
edmondk912 <edmondk912@gmail.com>
2019-10-23 13:14:26 -0400

    Deleted extraneous files and fixed import statement

--- frontEnd/lib/main.dart
+++ frontEnd/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:milestone_3/AddValuePair.dart';
+import 'AddValuePair.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
