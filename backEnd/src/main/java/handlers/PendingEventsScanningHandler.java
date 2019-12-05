c45e59e6cbf0a07071764a2f6ca650e7d8c04e9b
john-andrews12 <jhandrews12@gmail.com>
2019-12-04 20:35:10 -0500

    The scanner and dumb resolution algorithm work

---
+++ backEnd/src/main/java/handlers/PendingEventsScanningHandler.java
@@ -0,0 +1,20 @@
+package handlers;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import imports.PendingEventsManager;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class PendingEventsScanningHandler implements RequestStreamHandler {
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        try {
+            String scannerId = System.getenv("ScannerId");
+            PendingEventsManager.scanPendingEvents(scannerId);
+        } catch (Exception e) {
+            //TODO add log message https://github.com/SCCapstone/decision_maker/issues/82
+        }
+    }
+}
