775b407167c8362030daf9494024c188a5569472
John H Andrews <43245184+john-andrews12@users.noreply.github.com>
2020-01-14 07:59:49 -0500

    enabled backend active user emulation to make testing easier (#181)

--- backEnd/src/main/java/utilities/GetActiveUser.java
+++ backEnd/src/main/java/utilities/GetActiveUser.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.auth0.jwk.Jwk;
 import com.auth0.jwk.JwkException;
@@ -8,17 +9,33 @@ import com.auth0.jwk.UrlJwkProvider;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.common.collect.ImmutableList;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 import java.util.Map;
 
 public class GetActiveUser {
 
   public static final String PUBLIC_RSA_KEY_URL = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_ebbPP76nO/.well-known/jwks.json";
+  public static final List LIVE_FUNCTIONS = ImmutableList
+      .of("UsersPostEndpoint", "GroupsPostEndpoint", "CategoriesPostEndpoint");
+  public static final String EMULATED_ACTIVE_USER_KEY = "EMULATED_ACTIVE_USER";
 
-  public static String getActiveUserFromRequest(APIGatewayProxyRequestEvent request)
+  public static String getActiveUserFromRequest(APIGatewayProxyRequestEvent request,
+      Context context)
       throws JwkException, MalformedURLException {
+    String functionName = context.getFunctionName();
+
+    if (!LIVE_FUNCTIONS.contains(functionName)) {
+      String emulatedActiveUser = System.getenv(EMULATED_ACTIVE_USER_KEY);
+
+      if (emulatedActiveUser != null) {
+        return emulatedActiveUser;
+      }
+    }
+
     Map<String, String> headers = request.getHeaders();
     String authorization = headers.get("Authorization");
 
