3f3823b82a3df2b7cb90f9a8d5139ac9d4d98fda
john-andrews12 <jhandrews12@gmail.com>
2020-01-18 11:18:47 -0500

    updated groups post handler to proxy implementation

--- backEnd/src/main/java/handlers/GroupsPostHandler.java
+++ backEnd/src/main/java/handlers/GroupsPostHandler.java
@@ -2,70 +2,78 @@ package handlers;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import imports.DatabaseManagers;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Map;
-import utilities.IOStreamsHelper;
+import utilities.GetActiveUser;
 import utilities.JsonParsers;
 import utilities.Metrics;
+import utilities.RequestFields;
 import utilities.ResultStatus;
 
-public class GroupsPostHandler implements RequestStreamHandler {
+public class GroupsPostHandler implements
+    RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-  public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-      throws IOException {
+  public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request,
+      Context context) {
     ResultStatus resultStatus = new ResultStatus();
     Metrics metrics = new Metrics(context.getAwsRequestId());
     LambdaLogger lambdaLogger = context.getLogger();
 
     try {
-      Map<String, Object> jsonMap = JsonParsers.parseInput(inputStream);
+      Map<String, Object> jsonMap = JsonParsers.parseInput(request.getBody());
 
       if (!jsonMap.isEmpty()) {
         if (jsonMap.containsKey("action") && jsonMap.containsKey("payload")) {
           try {
-            String action = (String) jsonMap.get("action");
             Map<String, Object> payloadJsonMap = (Map<String, Object>) jsonMap.get("payload");
 
-            if (action.equals("getGroups")) {
-              resultStatus = DatabaseManagers.GROUPS_MANAGER
-                  .getGroups(payloadJsonMap, metrics, lambdaLogger);
-            } else if (action.equals("createNewGroup")) {
-              resultStatus = DatabaseManagers.GROUPS_MANAGER.createNewGroup(payloadJsonMap);
-            } else if (action.equals("editGroup")) {
-              resultStatus = DatabaseManagers.GROUPS_MANAGER.editGroup(payloadJsonMap);
-            } else if (action.equals("newEvent")) {
-              resultStatus = DatabaseManagers.GROUPS_MANAGER.newEvent(payloadJsonMap);
-            } else if (action.equals("optUserInOut")) {
-              resultStatus = DatabaseManagers.GROUPS_MANAGER.optInOutOfEvent(payloadJsonMap);
-            } else {
-              resultStatus = new ResultStatus(false, "Error: Invalid action entered");
-            }
+            if (!payloadJsonMap.containsKey(RequestFields.ACTIVE_USER)) {
+              payloadJsonMap
+                  .put(RequestFields.ACTIVE_USER,
+                      GetActiveUser.getActiveUserFromRequest(request, context));
 
-            IOStreamsHelper.writeToOutput(outputStream, resultStatus.toString());
+              String action = (String) jsonMap.get("action");
+
+              if (action.equals("getGroups")) {
+                resultStatus = DatabaseManagers.GROUPS_MANAGER
+                    .getGroups(payloadJsonMap, metrics, lambdaLogger);
+              } else if (action.equals("createNewGroup")) {
+                resultStatus = DatabaseManagers.GROUPS_MANAGER.createNewGroup(payloadJsonMap);
+              } else if (action.equals("editGroup")) {
+                resultStatus = DatabaseManagers.GROUPS_MANAGER.editGroup(payloadJsonMap);
+              } else if (action.equals("newEvent")) {
+                resultStatus = DatabaseManagers.GROUPS_MANAGER.newEvent(payloadJsonMap);
+              } else if (action.equals("optUserInOut")) {
+                resultStatus = DatabaseManagers.GROUPS_MANAGER.optInOutOfEvent(payloadJsonMap);
+              } else {
+                resultStatus.resultMessage = "Error: Invalid action entered";
+              }
+            } else {
+              resultStatus.resultMessage = "Error: Invalid key in payload.";
+            }
           } catch (Exception e) {
-            IOStreamsHelper.writeToOutput(outputStream,
-                new ResultStatus(false, "Error: Unable to parse request in handler.").toString());
+            resultStatus.resultMessage = "Error: Unable to parse request in handler.";
           }
         } else {
           //probably want to log this somewhere as front end validation shouldn't have let this through
           //TODO add log message https://github.com/SCCapstone/decision_maker/issues/82
-          IOStreamsHelper.writeToOutput(outputStream,
-              new ResultStatus(false, "Error: No action/payload entered.").toString());
+          resultStatus.resultMessage = "Error: No action/payload entered.";
         }
       } else {
         //probably want to log this somewhere as front end validation shouldn't have let this through
         //TODO add log message https://github.com/SCCapstone/decision_maker/issues/82
-        IOStreamsHelper.writeToOutput(outputStream,
-            new ResultStatus(false, "Error: No data entered.").toString());
+        resultStatus.resultMessage = "Error: No data entered.";
       }
     } catch (Exception e) {
       //TODO add log message https://github.com/SCCapstone/decision_maker/issues/82
-      IOStreamsHelper.writeToOutput(outputStream,
-          new ResultStatus(false, "Error: Unable to handle request.").toString());
+      resultStatus.resultMessage = "Error: Unable to handle request.";
     }
+
+    metrics.logMetrics(lambdaLogger);
+
+    return new APIGatewayProxyResponseEvent().withBody(resultStatus.toString());
   }
 }
