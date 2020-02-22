02b024cf20b9cabdaa5d047df6957e9bfc8ef730
joshrap67 <ghosthippo67@gmail.com>
2020-02-22 00:00:31 -0500

    Changed bottom nav bar color

--- frontEnd/lib/main.dart
+++ frontEnd/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:frontEnd/groups_widgets/groups_home.dart';
 import 'package:frontEnd/login_page.dart';
 import 'package:provider/provider.dart';
@@ -26,35 +27,39 @@ class MyApp extends StatelessWidget {
         //the asynchronous function hasValidTokensSet being able to fully execute
         //and return a Future<bool>.
         color: Color(0xff303030),
-        child: FutureBuilder<bool>(
-            future: hasValidTokensSet(),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              final ThemeNotifier themeNotifier =
-                  Provider.of<ThemeNotifier>(context);
-              if (!snapshot.hasData) {
-                return Center(
-                    child: CircularProgressIndicator(
-                        valueColor:
-                            AlwaysStoppedAnimation<Color>(Color(0xff5ce080))));
-              } else {
-                //If and only if the tokens are not valid or don't exist, open the login page.
-                if (!snapshot.data) {
-                  return MaterialApp(
-                    home: SignInPage(),
-                    theme: themeNotifier.getTheme(),
-                    title: "Pocket Poll",
-                  );
+        child: AnnotatedRegion(
+          // make the bottom navigation bar black instead of default white
+          value: SystemUiOverlayStyle.dark,
+          child: FutureBuilder<bool>(
+              future: hasValidTokensSet(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                final ThemeNotifier themeNotifier =
+                    Provider.of<ThemeNotifier>(context);
+                if (!snapshot.hasData) {
+                  return Center(
+                      child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Color(0xff5ce080))));
                 } else {
-                  return MaterialApp(
-                    home: GroupsHome(),
-                    theme: (Globals.user.appSettings.darkTheme)
-                        ? Globals.darkTheme
-                        : Globals.lightTheme,
-                    title: "Pocket Poll",
-                  );
+                  //If and only if the tokens are not valid or don't exist, open the login page.
+                  if (!snapshot.data) {
+                    return MaterialApp(
+                      home: SignInPage(),
+                      theme: themeNotifier.getTheme(),
+                      title: "Pocket Poll",
+                    );
+                  } else {
+                    return MaterialApp(
+                      home: GroupsHome(),
+                      theme: (Globals.user.appSettings.darkTheme)
+                          ? Globals.darkTheme
+                          : Globals.lightTheme,
+                      title: "Pocket Poll",
+                    );
+                  }
                 }
-              }
-            }));
+              }),
+        ));
   }
 }
 
