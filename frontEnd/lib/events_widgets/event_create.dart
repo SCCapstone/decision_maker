2fa9d3add093a221e231267a5e392b8943cf9916
Joshua Rapoport <36081235+joshrap67@users.noreply.github.com>
2020-03-07 17:07:24 -0500

    Event create changes (#358)
    Event create cleanup

--- frontEnd/lib/events_widgets/event_create.dart
+++ frontEnd/lib/events_widgets/event_create.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:frontEnd/imports/result_status.dart';
 import 'package:frontEnd/utilities/utilities.dart';
@@ -42,21 +44,25 @@ class _CreateEventState extends State<CreateEvent> {
   DateTime votingStart;
   DateTime votingEnd;
 
-  final int textFieldLength = 5;
+  final int textFieldLength = 4;
   bool willRsvp = true;
   String rsvpLabelText;
   String skipButtonText;
+  Timer timer;
 
   @override
   void dispose() {
     eventNameController.dispose();
     votingDurationController.dispose();
     rsvpDurationController.dispose();
+    timer?.cancel();
     super.dispose();
   }
 
   @override
   void initState() {
+    // every 15 seconds refresh the calculated times
+    timer = Timer.periodic(Duration(seconds: 15), (Timer t) => refreshTime());
     eventStartDate = convertDateToString(currentDate);
     eventStartTime = (currentTime.hour + 1).toString() + ":00";
     if ((currentTime.hour + 1) < 10) {
@@ -109,67 +115,73 @@ class _CreateEventState extends State<CreateEvent> {
                       decoration:
                           InputDecoration(labelText: "Enter an event name"),
                     ),
-                    Container(height: 20),
-                    // Temporarily using Containers to space out the elements
+                    Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height * .01),
+                    ),
                     Row(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: <Widget>[
                           Text("Start date and time for the event",
                               style: TextStyle(fontSize: 16)),
                         ]),
-                    Container(height: 10),
+                    Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height * .01),
+                    ),
                     Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: <Widget>[
-                          Container(
-                              width: 110,
-                              height: 40,
-                              child: RaisedButton(
-                                  onPressed: () {
-                                    selectStartDate(context);
-                                  },
-                                  child: Text(eventStartDateFormatted))),
-                          Container(
-                              width: 110,
-                              height: 40,
-                              child: RaisedButton(
-                                  onPressed: () {
-                                    selectStartTime(context);
-                                  },
-                                  child: Text(eventStartTimeFormatted))),
+                          RaisedButton(
+                              onPressed: () {
+                                selectStartDate(context);
+                              },
+                              child: Text(eventStartDateFormatted)),
+                          RaisedButton(
+                              onPressed: () {
+                                selectStartTime(context);
+                              },
+                              child: Text(eventStartTimeFormatted)),
                         ]),
-                    Container(height: 20),
+                    Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height * .01),
+                    ),
                     RaisedButton(
                       child: Text(getCategoryButtonMessage()),
                       onPressed: () {
                         showCategoriesPopup();
                       },
                     ),
-                    Container(height: 10),
+                    Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height * .005),
+                    ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Container(
-                          width: MediaQuery.of(context).size.width * .6,
-                          child: TextFormField(
-                            controller: rsvpDurationController,
-                            keyboardType: TextInputType.number,
-                            enabled: willRsvp,
-                            validator: validRsvpDuration,
-                            maxLength: textFieldLength,
-                            onChanged: (String arg) {
-                              // if already at max length and you keep typing, setState won't get called again
-                              if (!(arg.length == textFieldLength &&
-                                  rsvpDuration.length == textFieldLength)) {
-                                rsvpDuration = arg;
-                                setState(() {});
-                              }
-                            },
-                            decoration:
-                                InputDecoration(
-                                    labelText: rsvpLabelText,
-                                    counterText: ""
-                                ),
+                        Visibility(
+                          visible: willRsvp,
+                          child: Expanded(
+                            child: Container(
+                              child: TextFormField(
+                                controller: rsvpDurationController,
+                                keyboardType: TextInputType.number,
+                                enabled: willRsvp,
+                                validator: validRsvpDuration,
+                                maxLength: textFieldLength,
+                                onChanged: (String arg) {
+                                  // if already at max length and you keep typing, setState won't get called again
+                                  if (!(arg.length == textFieldLength &&
+                                      rsvpDuration.length == textFieldLength)) {
+                                    rsvpDuration = arg;
+                                    setState(() {});
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                    labelText: rsvpLabelText, counterText: ""),
+                              ),
+                            ),
                           ),
                         ),
                         RaisedButton(
@@ -180,7 +192,6 @@ class _CreateEventState extends State<CreateEvent> {
                                 rsvpDurationController.text = "0";
                                 rsvpDuration = rsvpDurationController.text;
                                 skipButtonText = "Set RSVP";
-                                rsvpLabelText = "Skipping RSVP phase";
                               } else {
                                 rsvpDurationController.text = Globals
                                     .currentGroup.defaultRsvpDuration
@@ -190,35 +201,58 @@ class _CreateEventState extends State<CreateEvent> {
                                 rsvpLabelText = "RSVP Duration (in minutes)";
                               }
                               setState(() {});
-                            })
+                            }),
+                        Padding(
+                          // this is hacky af, but otherwise when textfield is hidden the button jumps to the right
+                          padding: EdgeInsets.all(
+                              MediaQuery.of(context).size.height * .000),
+                        ),
                       ],
                     ),
-                    TextFormField(
-                      controller: votingDurationController,
-                      keyboardType: TextInputType.number,
-                      validator: validVotingDuration,
-                      maxLength: textFieldLength,
-                      onChanged: (String arg) {
-                        if (!(arg.length == textFieldLength &&
-                            votingDuration.length == textFieldLength)) {
-                          votingDuration = arg;
-                          setState(() {});
-                        }
-                      },
-                      decoration: InputDecoration(
-                          labelText: "Voting duration (in minutes)",
-                          counterText: ""
-                      ),
+                    Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height * .01),
                     ),
                     Text(
                       calculateVotingStartDateTime(),
+                      textAlign: TextAlign.center,
                       style: TextStyle(
                           fontSize:
                               DefaultTextStyle.of(context).style.fontSize *
                                   0.35),
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            child: TextFormField(
+                              controller: votingDurationController,
+                              keyboardType: TextInputType.number,
+                              validator: validVotingDuration,
+                              maxLength: textFieldLength,
+                              onChanged: (String arg) {
+                                if (!(arg.length == textFieldLength &&
+                                    votingDuration.length == textFieldLength)) {
+                                  votingDuration = arg;
+                                  setState(() {});
+                                }
+                              },
+                              decoration: InputDecoration(
+                                  labelText: "Voting Duration (in minutes)",
+                                  counterText: ""),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height * .01),
+                    ),
                     Text(
                       calculateVotingEndDateTime(),
+                      textAlign: TextAlign.center,
                       style: TextStyle(
                           fontSize:
                               DefaultTextStyle.of(context).style.fontSize *
@@ -232,11 +266,6 @@ class _CreateEventState extends State<CreateEvent> {
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-              RaisedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Cancel")),
               RaisedButton.icon(
                   onPressed: () {
                     if (categorySelected.isEmpty) {
@@ -250,6 +279,11 @@ class _CreateEventState extends State<CreateEvent> {
             ])),
       ),
     );
+  }
+
+  void refreshTime() {
+    // used to get the most recent calculated times in case user is just idle on this screen
+    setState(() {});
   }
 
   void showCategoriesPopup() {
@@ -310,9 +344,11 @@ class _CreateEventState extends State<CreateEvent> {
   }
 
   bool startTimeIsInvalid(DateTime startDate, TimeOfDay startTime) {
-    // Check if the start time is on the earlier in the day than the current
-    // time (in other words, same date as current day && earlier time means
-    // the start time is invalid)
+    /*
+      Check if the start time is on the earlier in the day than the current
+      time (in other words, same date as current day && earlier time means
+      the start time is invalid)
+     */
     currentDate = DateTime.now();
     currentTime = TimeOfDay.now();
     return ((startDate.year == currentDate.year &&
@@ -324,29 +360,36 @@ class _CreateEventState extends State<CreateEvent> {
   }
 
   String calculateVotingStartDateTime() {
+    String displayVal;
+
     if (rsvpDuration == "0") {
       votingStart = DateTime.now();
-      return "Voting will start immediately.";
+      displayVal = Globals.formatter.format(votingStart);
     } else {
       if (validRsvpDuration(rsvpDuration) != null) {
-        return "RSVP duration invalid: " + validRsvpDuration(rsvpDuration);
+        displayVal = Globals.formatter.format(votingStart);
+      } else {
+        votingStart = DateTime.now();
+        votingStart =
+            votingStart.add(Duration(minutes: int.parse(rsvpDuration)));
+        displayVal = Globals.formatter.format(votingStart);
       }
-      votingStart = DateTime.now();
-      votingStart = votingStart.add(Duration(minutes: int.parse(rsvpDuration)));
-      return "Voting will start " + Globals.formatter.format(votingStart);
     }
+    return "Voting will start: $displayVal";
   }
 
   String calculateVotingEndDateTime() {
+    String displayVal;
+
     if (validVotingDuration(votingDuration) != null) {
-      return "Voting duration invalid: " + validVotingDuration(votingDuration);
-    }
-    if (votingStart != null) {
+      displayVal = Globals.formatter.format(votingEnd);
+    } else if (votingStart != null) {
       votingEnd = votingStart.add(Duration(minutes: int.parse(votingDuration)));
-      return "Voting will end " + Globals.formatter.format(votingEnd);
+      displayVal = Globals.formatter.format(votingEnd);
     } else {
-      return "";
+      displayVal = "";
     }
+    return "Voting will end: $displayVal";
   }
 
   void validateInput() async {
@@ -357,12 +400,12 @@ class _CreateEventState extends State<CreateEvent> {
       if (this.votingStart.isAfter(
           DateTime.parse(this.eventStartDate + " " + this.eventStartTime))) {
         errorMessage +=
-            "RSVP duration invalid: RSVP will end after the event starts\n";
+            "RSVP duration invalid: RSVP will end after the event starts\n\n";
       }
       if (this.votingEnd.isAfter(
           DateTime.parse(this.eventStartDate + " " + this.eventStartTime))) {
         errorMessage +=
-            "Voting duration invalid: voting will end after the event starts";
+            "Voting duration invalid: Voting will end after the event starts";
       }
       if (errorMessage != "") {
         showErrorMessage("Error", errorMessage, context);
