735a16ca6573e931fb1258d6c0be506b26ed8ba5
john-andrews12 <jhandrews12@gmail.com>
2020-03-16 21:59:59 -0400

    clean up

--- backEnd/src/main/java/models/User.java
+++ backEnd/src/main/java/models/User.java
@@ -4,16 +4,10 @@ import imports.UsersManager;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder(toBuilder = true)
 public class User {
 
   private String username;
