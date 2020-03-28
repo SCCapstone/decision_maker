29e619b241cb02eb35ebcb4efc0ad8b414c1f798
John H Andrews <43245184+john-andrews12@users.noreply.github.com>
2020-03-28 17:35:27 -0400

    Validation to 'updateUserRatings' and 'updateUserSettings'. We now merged existing ratings with new ratings. (#424)

--- backEnd/src/main/java/models/AppSettings.java
+++ backEnd/src/main/java/models/AppSettings.java
@@ -1,11 +1,16 @@
 package models;
 
+import com.google.common.collect.ImmutableList;
+import exceptions.InvalidAttributeValueException;
 import imports.UsersManager;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
 @Builder
@@ -14,11 +19,14 @@ public class AppSettings {
 
   //TODO at some point update the ints to bools
   private boolean darkTheme;
-  private Integer groupSort;
-  private Integer categorySort;
   private boolean muted;
 
-  public static AppSettings defaultSettings() {
+  @Setter(AccessLevel.NONE)
+  private Integer groupSort;
+  @Setter(AccessLevel.NONE)
+  private Integer categorySort;
+
+  public static AppSettings defaultSettings() throws InvalidAttributeValueException {
     return AppSettings.builder()
         .darkTheme(UsersManager.DEFAULT_DARK_THEME)
         .groupSort(UsersManager.DEFAULT_GROUP_SORT)
@@ -27,13 +35,35 @@ public class AppSettings {
         .build();
   }
 
-  public AppSettings(final Map<String, Object> jsonMap) {
+  public AppSettings(final Map<String, Object> jsonMap) throws InvalidAttributeValueException {
     if (jsonMap != null) {
       this.setDarkTheme(this.getBoolFromObject(jsonMap.get(UsersManager.APP_SETTINGS_DARK_THEME)));
       this.setGroupSort(this.getIntFromObject(jsonMap.get(UsersManager.APP_SETTINGS_GROUP_SORT)));
       this.setCategorySort(
           this.getIntFromObject(jsonMap.get(UsersManager.APP_SETTINGS_CATEGORY_SORT)));
       this.setMuted(this.getBoolFromObject(jsonMap.get(UsersManager.APP_SETTINGS_MUTED)));
+    }
+  }
+
+  private void setGroupSort(final Integer groupSort) throws InvalidAttributeValueException {
+    // these values decode to have meaning in globals.dart on the front end
+    final List<Integer> validGroupSortValues = ImmutableList.of(0, 1, 2, 3);
+    if (validGroupSortValues.contains(groupSort)) {
+      this.groupSort = groupSort;
+    } else {
+      throw new InvalidAttributeValueException(UsersManager.APP_SETTINGS_GROUP_SORT,
+          validGroupSortValues, groupSort);
+    }
+  }
+
+  private void setCategorySort(final Integer categorySort) throws InvalidAttributeValueException {
+    // these values decode to have meaning in globals.dart on the front end
+    final List<Integer> validCategorySortValues = ImmutableList.of(1, 2);
+    if (validCategorySortValues.contains(categorySort)) {
+      this.categorySort = categorySort;
+    } else {
+      throw new InvalidAttributeValueException(UsersManager.APP_SETTINGS_CATEGORY_SORT,
+          validCategorySortValues, categorySort);
     }
   }
 
