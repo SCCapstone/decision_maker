a3f123d9650a7c51cf45fa4dab62f51ae7bfc85b
john-andrews12 <jhandrews12@gmail.com>
2020-04-01 01:10:02 -0400

    working

---
+++ backEnd/src/main/java/DataCruncher.java
@@ -0,0 +1,273 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import models.Category;
+import models.Group;
+import models.User;
+import utilities.ErrorDescriptor;
+
+public class DataCruncher {
+
+  private LinkedHashMap<String, Map<Integer, Integer>> controlRatingCountsByChoice;
+  private LinkedHashMap<String, Map<Integer, Integer>> allRatingCountsByChoice;
+
+  private Group group;
+  private Category category;
+  private Map<String, User> allUsers;
+  private Float k;
+  private int totalDifferentThanControl;
+
+  public DataCruncher(final Group group, final Category category,
+      final Map<String, User> allUsers, final Float k) {
+    this.group = group;
+    this.category = category;
+    this.allUsers = allUsers;
+    this.k = k;
+    this.totalDifferentThanControl = 0;
+
+    //set up the default mappings
+    this.resetControlRatingsCountsByChoice();
+    this.resetAllRatingsCountsByChoice();
+  }
+
+  public void crunch() {
+    //first setup the control values
+    List<Map<String, Integer>> allCategoryChoiceRatings = new ArrayList<>();
+    for (String username : this.group.getMembers().keySet()) {
+      try {
+        final User user = this.allUsers.get(username);
+
+        final Map<String, Integer> categoryChoiceRatings = user.getCategoryRatings()
+            .get(this.category.getCategoryId());
+
+        allCategoryChoiceRatings.add(categoryChoiceRatings);
+      } catch (final Exception e) {
+        System.out.println(new ErrorDescriptor<>(username, "blah", e));
+      }
+    }
+
+    //add in the ratings!
+    this.clearAndSetControlRatingCountsByChoice(allCategoryChoiceRatings);
+
+    //Now we need to get the random users or at least the random categoryChoiceRating maps
+    for (int i = 0; i < Math.ceil(this.k * group.getMembers().size()); i++) {
+      allCategoryChoiceRatings.add(this.getRandomUserChoiceRatings());
+    }
+
+    this.clearAndSetAllRatingCountsByChoice(allCategoryChoiceRatings);
+
+    this.updateTotalDifferent();
+  }
+
+  public void updateTotalDifferent() {
+    List<String> control = this.getTopThreeControlChoices(true);
+    List<String> all = this.getTopThreeAllChoices(true);
+
+    this.totalDifferentThanControl += this.getNumberOfChanges(control, all);
+  }
+
+  //we put one additional vote into ratings 1 through 5 (so everything besides 0)
+  //we then roll a random number and find what rating that correlates to probabilistically
+  public Map<String, Integer> getRandomUserChoiceRatings() {
+    Map<String, Integer> categoryChoiceRatings = new HashMap<>();
+    Random random = new Random();
+
+    for (String choiceId : this.controlRatingCountsByChoice.keySet()) {
+      int randInt = random.nextInt(100); // [0, 100)
+      int runningProb = 0;
+      int totalVotes =
+          this.group.getMembers().size() + 5; //we added one vote to each of the choices 1 through 5
+
+      Map<Integer, Integer> ratingCounts = this.controlRatingCountsByChoice.get(choiceId);
+
+      for (int rating = 0; rating < 6; rating++) {
+        int controlCount = ratingCounts.get(rating);
+        int topOfRange = runningProb;
+
+        //we have to figure out how much percentage this choice had
+        if (rating == 0) {
+          // we handle zero differently just because
+          topOfRange += Math.floor(controlCount * 100.0 / totalVotes);
+        } else {
+          topOfRange += Math.floor((controlCount + 1) * 100.0 / totalVotes);
+        }
+
+        if (randInt < topOfRange) {
+          categoryChoiceRatings.putIfAbsent(choiceId, rating);
+          break;
+        }
+        runningProb = topOfRange + 1;
+      }
+
+      if (!categoryChoiceRatings.containsKey(choiceId)) {
+        System.out.println("didn't contain " + choiceId);
+        categoryChoiceRatings.put(choiceId, 3);
+      }
+    }
+
+    return categoryChoiceRatings;
+  }
+
+  private void clearAndSetControlRatingCountsByChoice(
+      List<Map<String, Integer>> allCategoryChoiceRatings) {
+    this.resetControlRatingsCountsByChoice();
+
+    for (Map<String, Integer> categoryChoiceRatings : allCategoryChoiceRatings) {
+      for (String choiceId : this.controlRatingCountsByChoice.keySet()) {
+        Integer selectedRating;
+        if (categoryChoiceRatings != null && categoryChoiceRatings.containsKey(choiceId)) {
+          //increment the old count of this rating value by 1
+          selectedRating = categoryChoiceRatings.get(choiceId);
+        } else {
+          selectedRating = 3;
+        }
+
+        //increment the old count of this rating value by 1
+        final Integer newCount =
+            this.controlRatingCountsByChoice.get(choiceId).get(selectedRating) + 1;
+
+        this.controlRatingCountsByChoice.get(choiceId).replace(selectedRating, newCount);
+      }
+    }
+  }
+
+  private void clearAndSetAllRatingCountsByChoice(
+      List<Map<String, Integer>> allCategoryChoiceRatings) {
+    this.resetAllRatingsCountsByChoice();
+
+    for (Map<String, Integer> categoryChoiceRatings : allCategoryChoiceRatings) {
+      for (String choiceId : this.allRatingCountsByChoice.keySet()) {
+        Integer selectedRating;
+        if (categoryChoiceRatings != null && categoryChoiceRatings.containsKey(choiceId)) {
+          //increment the old count of this rating value by 1
+          selectedRating = categoryChoiceRatings.get(choiceId);
+        } else {
+          selectedRating = 3;
+        }
+
+        //increment the old count of this rating value by 1
+        final Integer newCount =
+            this.allRatingCountsByChoice.get(choiceId).get(selectedRating) + 1;
+
+        this.allRatingCountsByChoice.get(choiceId).replace(selectedRating, newCount);
+      }
+    }
+  }
+
+  private void resetControlRatingsCountsByChoice() {
+    this.controlRatingCountsByChoice = new LinkedHashMap<>();
+    for (String choiceId : category.getChoices().keySet()) {
+      this.controlRatingCountsByChoice
+          .putIfAbsent(choiceId, this.getEmptyRatingCountsByChoiceMap());
+    }
+  }
+
+  private void resetAllRatingsCountsByChoice() {
+    this.allRatingCountsByChoice = new LinkedHashMap<>();
+    for (String choiceId : category.getChoices().keySet()) {
+      this.allRatingCountsByChoice
+          .putIfAbsent(choiceId, this.getEmptyRatingCountsByChoiceMap());
+    }
+  }
+
+  private Map<Integer, Integer> getEmptyRatingCountsByChoiceMap() {
+    return new HashMap<Integer, Integer>() {{
+      put(0, 0);
+      put(1, 0);
+      put(2, 0);
+      put(3, 0);
+      put(4, 0);
+      put(5, 0);
+    }};
+  }
+
+  public int getSumOfRatingsCounts(Map<Integer, Integer> ratingsToCount) {
+    int ret = 0;
+
+    for (int rating = 0; rating < 6; rating++) {
+      ret += ratingsToCount.get(rating) * rating; // count * rating
+    }
+
+    return ret;
+  }
+
+  List<String> getTopThreeControlChoices(boolean justKeys) {
+    return this.controlRatingCountsByChoice.entrySet().stream()
+        .sorted((e1, e2) ->
+            this.getSumOfRatingsCounts(e1.getValue()) > this.getSumOfRatingsCounts(e2.getValue())
+                ? -1 : 1)
+        .limit(3)
+        .map((e1) -> justKeys ? e1.getKey() : String.format("%s:%s", e1.getKey(), this.getSumOfRatingsCounts(e1.getValue())))
+        .collect(Collectors.toList());
+  }
+
+  List<String> getTopThreeAllChoices(boolean justKeys) {
+    return this.allRatingCountsByChoice.entrySet().stream()
+        .sorted((e1, e2) ->
+            this.getSumOfRatingsCounts(e1.getValue()) > this.getSumOfRatingsCounts(e2.getValue())
+                ? -1 : 1)
+        .limit(3)
+        .map((e1) -> justKeys ? e1.getKey() : String.format("%s:%s", e1.getKey(), this.getSumOfRatingsCounts(e1.getValue())))
+        .collect(Collectors.toList());
+  }
+
+  public String toString() {
+    String tableString = String
+        .format("%s/%s/%s,", this.group.getGroupId(), this.category.getCategoryId(),
+            this.k.toString());
+    tableString += String.format("%s,", this.getTopThreeControlChoices(false));
+    tableString += String.format("%s", this.getTopThreeAllChoices(false));
+    return tableString;
+  }
+
+  public String toString2() {
+    String tableString = String
+        .format("%s/%s/%s,", this.group.getGroupId(), this.category.getCategoryId(),
+            this.k.toString());
+    List<String> control = this.getTopThreeControlChoices(true);
+    List<String> all = this.getTopThreeAllChoices(true);
+    tableString += String.format("%s-%s-%s,", control.get(0), control.get(1), control.get(2));
+    tableString += String.format("%s-%s-%s,", all.get(0), all.get(1), all.get(2));
+
+//    if (control.containsAll(all)) {
+//      tableString += "0";
+//    } else {
+//      tableString += "1";
+//    }
+
+    tableString += this.getNumberOfChanges(control, all);
+
+    return tableString;
+  }
+
+  public String getExperimentResults() {
+    String tableString = "";
+    List<String> control = this.getTopThreeControlChoices(true);
+    List<String> all = this.getTopThreeAllChoices(true);
+    tableString += String.format("%s-%s-%s,", all.get(0), all.get(1), all.get(2));
+
+    tableString += this.getNumberOfChanges(control, all);
+
+    return tableString;
+  }
+
+  public int getTotalDifferentThanControl() {
+    return this.totalDifferentThanControl;
+  }
+
+  private int getNumberOfChanges(List<String> control, List<String> all) {
+    int ret = 0;
+
+    for (String s: all) {
+      if (!control.contains(s)) {
+        ret++;
+      }
+    }
+
+    return ret;
+  }
+}
---
+++ backEnd/src/main/java/Ntsh.java
@@ -0,0 +1,300 @@
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.stream.Collectors;
+import models.Category;
+import models.Group;
+import models.User;
+import utilities.ErrorDescriptor;
+
+public class Ntsh {
+
+  public static void main(String[] args) {
+    //Setup the categories
+    final Category smallCategory = new Category(Collections.emptyMap()); // 6 choices
+    smallCategory.setCategoryId("smallCategory");
+    smallCategory.setChoices(new HashMap<String, String>() {{
+      put("a", "a");
+      put("b", "b");
+      put("c", "c");
+      put("d", "d");
+      put("e", "e");
+      put("f", "f");
+    }});
+
+    final Category mediumCategory = new Category(Collections.emptyMap()); // 10 choices
+    mediumCategory.setCategoryId("mediumCategory");
+    mediumCategory.setChoices(new HashMap<String, String>() {{
+      put("a", "a");
+      put("b", "b");
+      put("c", "c");
+      put("d", "d");
+      put("e", "e");
+      put("f", "f");
+      put("g", "g");
+      put("h", "h");
+      put("i", "i");
+      put("j", "j");
+    }});
+
+    final Category largeCategory = new Category(Collections.emptyMap()); // 15 choices
+    largeCategory.setCategoryId("largeCategory");
+    largeCategory.setChoices(new HashMap<String, String>() {{
+      put("a", "a");
+      put("b", "b");
+      put("c", "c");
+      put("d", "d");
+      put("e", "e");
+      put("f", "f");
+      put("g", "g");
+      put("h", "h");
+      put("i", "i");
+      put("j", "j");
+      put("k", "k");
+      put("l", "l");
+      put("m", "m");
+      put("n", "n");
+      put("o", "o");
+    }});
+
+    LinkedHashMap<String, User> users = new LinkedHashMap<>();
+    Random random = new Random();
+    try {
+      for (int i = 1; i <= 20; i++) {
+        User newUser = new User(Collections.emptyMap());
+        newUser.setUsername("user" + i);
+
+        Map<String, Object> userCategoryRatings = new HashMap<>();
+        //create random ratings for the 3 above categories
+        Map<String, Integer> smallChoiceRatings = new HashMap<>();
+        for (String choiceId : smallCategory.getChoices().keySet()) {
+          smallChoiceRatings.putIfAbsent(choiceId, random.nextInt(6)); // [0,6)
+        }
+        userCategoryRatings.putIfAbsent("smallCategory", smallChoiceRatings);
+
+        Map<String, Integer> mediumChoiceRatings = new HashMap<>();
+        for (String choiceId : mediumCategory.getChoices().keySet()) {
+          mediumChoiceRatings.putIfAbsent(choiceId, random.nextInt(6)); // [0,6)
+        }
+        userCategoryRatings.putIfAbsent("mediumCategory", mediumChoiceRatings);
+
+        Map<String, Integer> largeChoiceRatings = new HashMap<>();
+        for (String choiceId : largeCategory.getChoices().keySet()) {
+          largeChoiceRatings.putIfAbsent(choiceId, random.nextInt(6)); // [0,6)
+        }
+        userCategoryRatings.putIfAbsent("largeCategory", largeChoiceRatings);
+
+        newUser.setCategoryRatings(userCategoryRatings);
+        users.putIfAbsent(newUser.getUsername(), newUser);
+      }
+    } catch (final Exception e) {
+      System.out.println(new ErrorDescriptor<>("blah", "blah", e));
+    }
+
+    //Setup the groups
+    final Group smallGroup = new Group();
+    smallGroup.setGroupId("smallGroup");
+    Map<String, Object> smallGroupMembers = users.entrySet().stream().limit(5)
+        .collect(Collectors.toMap(
+            Entry::getKey, (e) -> e.getValue().asMember().asMap(), (e1, e2) -> e2, HashMap::new
+        ));
+    smallGroup.setMembers(smallGroupMembers);
+
+    final Group mediumGroup = new Group();
+    mediumGroup.setGroupId("mediumGroup");
+    Map<String, Object> mediumGroupMembers = users.entrySet().stream().limit(10)
+        .collect(Collectors.toMap(
+            Entry::getKey, (e) -> e.getValue().asMember().asMap(), (e1, e2) -> e2, HashMap::new
+        ));
+    mediumGroup.setMembers(mediumGroupMembers);
+
+    final Group largeGroup = new Group();
+    largeGroup.setGroupId("largeGroup");
+    Map<String, Object> largeGroupMembers = users.entrySet().stream().limit(20)
+        .collect(Collectors.toMap(
+            Entry::getKey, (e) -> e.getValue().asMember().asMap(), (e1, e2) -> e2, HashMap::new
+        ));
+    largeGroup.setMembers(largeGroupMembers);
+
+    List<Group> groups = new ArrayList<>();
+    groups.add(smallGroup);
+    groups.add(mediumGroup);
+    groups.add(largeGroup);
+
+    List<Category> categories = new ArrayList<>();
+    categories.add(smallCategory);
+    categories.add(mediumCategory);
+    categories.add(largeCategory);
+
+    List<Float> kValues = ImmutableList.of(0.1f, 0.15f, 0.2f);
+
+    int numberOfTests = 15;
+    if (numberOfTests < 1) { //one test results will always be in the output
+      numberOfTests = 1;
+    }
+    String tableHeader = "g/c/k,ctrl";
+
+    for (int i = 1; i <= numberOfTests; i++) {
+      tableHeader += ",test" + i + ",#changed?";
+    }
+
+    tableHeader += ",Total Changed";
+
+    System.out.println(tableHeader);
+
+    DataCruncher dataCruncher;
+
+    for (Group g: groups) {
+      for (Category c: categories) {
+        for (Float k: kValues) {
+          dataCruncher = new DataCruncher(g, c, users, k);
+          dataCruncher.crunch();
+          String tableRow = dataCruncher.toString2();
+
+          //limited by numberOfTest - 1 since the first test is done in the initial .crunch()
+          for (int i = 0; i < numberOfTests - 1; i++) {
+            dataCruncher.crunch();
+            tableRow += "," + dataCruncher.getExperimentResults();
+          }
+
+          tableRow += "," + dataCruncher.getTotalDifferentThanControl();
+
+          System.out.println(tableRow);
+        }
+      }
+    }
+
+//    dataCruncher = new DataCruncher(smallGroup, smallCategory, users, 0.1f);
+//    dataCruncher.crunch();
+//    System.out.println(dataCruncher.toString());
+//
+//    dataCruncher = new DataCruncher(mediumGroup, smallCategory, users, 0.1f);
+//    dataCruncher.crunch();
+//    System.out.println(dataCruncher.toString());
+//
+//    dataCruncher = new DataCruncher(largeGroup, smallCategory, users, 0.1f);
+//    dataCruncher.crunch();
+//    System.out.println(dataCruncher.toString());
+
+//    for (Group group : groups) {
+//      System.out.println("Group is " + group.getGroupId());
+//      for (Category category : categories) {
+//        System.out.println("Category is " + category.getCategoryId());
+//
+//        final LinkedHashMap<String, Object> returnValue = new LinkedHashMap<>();
+//
+//        //setup is done now - time to algorithm
+//        //build an array of user choice rating sums, start with all the current choice ids in the category
+//        final Map<String, Integer> choiceRatingsToSums = new HashMap<>();
+//        final Map<String, Map<Integer, Integer>> individualChoiceRatingSums = new HashMap<>();
+//        for (String choiceId : category.getChoices().keySet()) {
+//          choiceRatingsToSums.putIfAbsent(choiceId, 0);
+//          individualChoiceRatingSums.putIfAbsent(choiceId, new HashMap<Integer, Integer>() {{
+//            put(0, 0);
+//            put(1, 0);
+//            put(2, 0);
+//            put(3, 0);
+//            put(4, 0);
+//            put(5, 0);
+//          }});
+//        }
+//
+//
+//        //now we need to go through and add one to 10 percent random votes
+//        //sum all of the user ratings
+//        for (String username : group.getMembers().keySet()) {
+//          try {
+//            final User user = users.get(username);
+//
+//            final Map<String, Integer> categoryChoiceRatings = user.getCategoryRatings()
+//                .get(category.getCategoryId());
+//
+//            for (String choiceId : choiceRatingsToSums.keySet()) {
+//              if (categoryChoiceRatings != null && categoryChoiceRatings.containsKey(choiceId)) {
+//                choiceRatingsToSums.replace(choiceId,
+//                    choiceRatingsToSums.get(choiceId) + categoryChoiceRatings.get(choiceId));
+//              } else {
+//                choiceRatingsToSums.replace(choiceId, choiceRatingsToSums.get(choiceId) + 3);
+//              }
+//            }
+//          } catch (Exception e) {
+//            System.out.println(new ErrorDescriptor<>(username, "blah", e));
+//          }
+//        }
+//
+//        List<String> allValuesMapped = choiceRatingsToSums.entrySet().stream()
+//            .sorted((e1, e2) -> e1.getValue() > e2.getValue() ? -1 : 1)
+//            .map((e) -> e.getKey() + ": " + e.getValue())
+//            .collect(Collectors.toList());
+//
+//        //user ratings have been summed, get the top X now
+//        while (returnValue.size() < 3 && choiceRatingsToSums.size() > 0) {
+//          String maxKey = null;
+//          for (final String key : choiceRatingsToSums.keySet()) {
+//            if (maxKey == null || choiceRatingsToSums.get(key) > choiceRatingsToSums.get(maxKey)) {
+//              maxKey = key;
+//            }
+//          }
+//
+//          //we add to the return map and remove the max from the choice rating map
+//          returnValue.putIfAbsent(maxKey, category.getChoices().get(maxKey));
+//          choiceRatingsToSums.remove(maxKey);
+//        }
+//
+//        System.out.println("Max choices are: " + returnValue.values());
+//        System.out.println("All choices are: " + allValuesMapped.toString());
+//        System.out.println();
+//      }
+//      System.out.println();
+//    }
+
+    //Setup the users
+//    final User user1 = new User();
+//    user1.setUsername("user1");
+//    user1.setCategoryRatings(new HashMap<String, Object>(){{
+//      put("smallCategory", new HashMap<String, Integer>() {{
+//        put("a", 2);
+//        put("b", 3);
+//        put("c", 5);
+//        put("d", 2);
+//        put("e", 4);
+//        put("f", 4);
+//      }});
+//      put("mediumCategory", new HashMap<String, Integer>() {{
+//        put("a", 1);
+//        put("b", 4);
+//        put("c", 4);
+//        put("d", 2);
+//        put("e", 0);
+//        put("f", 4);
+//        put("g", 3);
+//        put("h", 5);
+//        put("i", 2);
+//        put("j", 4);
+//      }});
+//      put("largeCategory", new HashMap<String, Integer>() {{
+//        put("a", 1);
+//        put("b", 0);
+//        put("c", 4);
+//        put("d", 2);
+//        put("e", 4);
+//        put("f", 5);
+//        put("g", 4);
+//        put("h", 3);
+//        put("i", 2);
+//        put("j", 4);
+//        put("k", 4);
+//        put("l", 3);
+//        put("m", 5);
+//        put("n", 0);
+//        put("o", 2);
+//      }});
+//    }});
+  }
+}
