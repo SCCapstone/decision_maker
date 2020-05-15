11a56a511cad0f57ad8ea0f3ce3a5c9d8c5e7afc
joshrap67 <ghosthippo67@gmail.com>
2020-05-15 18:15:12 -0400

    onResume works now for notifications

--- front_end_pocket_poll/android/app/src/main/java/com/example/milestone_3/MainActivity.java
+++ front_end_pocket_poll/android/app/src/main/java/com/example/milestone_3/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.frontEnd;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.app.NotificationManager;
+import android.content.Context;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    // Removes sotifications on the notifcation tray
+    cancelAllNotifications();
+  }
+
+  private void cancelAllNotifications() {
+    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    notificationManager.cancelAll();
   }
 }
