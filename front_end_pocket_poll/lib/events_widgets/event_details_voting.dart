01f97f6fd6836e71e6f696eaa6fd2583e85203a4
joshrap67 <ghosthippo67@gmail.com>
2020-05-26 22:50:30 -0400

    Missed a spot

--- front_end_pocket_poll/lib/events_widgets/event_details_voting.dart
+++ front_end_pocket_poll/lib/events_widgets/event_details_voting.dart
@@ -212,7 +212,7 @@ class _EventDetailsVotingState extends State<EventDetailsVoting> {
                     controller: this.pageController,
                     count: this.choices.length,
                     effect: SlideEffect(
-                        dotColor: Colors.grey, activeDotColor: Colors.green),
+                        dotColor: Colors.grey, activeDotColor: Colors.blueAccent),
                   )
                 ],
               ),
